678b2a5eeea8e296c8188185604af90e05cb374d
M.S_Code <119150719+MohidevSahuCode@users.noreply.github.com>
2023-07-19 21:00:28 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,9 @@
+User_input1 = int(input("Enter the first number : "))
+User_input2 = int(input("Enter the second number : "))
+
+if User_input2 > User_input1 :
+    print("Second number is greater than first number .")
+elif User_input2 < User_input1 :
+    print("First number is greater than Second number .")
+elif User_input1 == User_input2 :
+    print("First number is equals to second number .")

17e115dce7df4a161d65c1a59ae3e8899a05fe9c
M.S_Code <119150719+MohidevSahuCode@users.noreply.github.com>
2023-07-21 08:08:55 +0530

    test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,7 @@
-User_input1 = int(input("Enter the first number : "))
-User_input2 = int(input("Enter the second number : "))
+a = 33
+b = "33"
 
-if User_input2 > User_input1 :
-    print("Second number is greater than first number .")
-elif User_input2 < User_input1 :
-    print("First number is greater than Second number .")
-elif User_input1 == User_input2 :
-    print("First number is equals to second number .")
+if b > a :
+    print("b is greater than a .")
+elif a == b :
+    print("a is equals to b .")
